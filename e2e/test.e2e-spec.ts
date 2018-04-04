1093e7eeeb4a3b505faaa51790969aa5383ed593
amit shukla <amit.shukla@igglobal.com>
2018-04-04 12:31:50 +0530

    login test for e2e

--- e2e/test.e2e-spec.ts
+++ e2e/test.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { browser, element, by, ElementFinder } from 'protractor';
 
-let clickerField: ElementFinder = element(by.css('.text-input'));
  
 describe('Example E2E Test', () => {
  
@@ -15,35 +14,6 @@ describe('Example E2E Test', () => {
         .toContain('Home'); // Check if it contains the text "Home"
  
   });
-
-  it('has an input box for user name', () => {
-    element(by.css('.text-input')).isPresent().then(present => expect(present).toEqual(true));
-  });
- 
-  it('has an input box for user password', () => {
-    var foo = element(by.id('password'));
-      expect(foo.getAttribute('type')).toEqual('password');
-  });
-
-  it('has an login button', () => {
-    var foo = element(by.id('loginButton'));
-      expect(foo.getAttribute('color')).toEqual('danger');
-  });
-
-  it('has text on login button', () => {
-    var foo = element(by.id('loginButton'));
-    expect(foo.getText()).toEqual('LOGIN');
-  });
-
-  it('should navigate to other page', () => {
-    // 'shukla.amit2222@gmail.com'.split('').forEach((c) => element(by.css('.text-input')).sendKeys(c));
-    element(by.css('.text-input')).sendKeys("sagar");
-    'shukla.amit'.split('').forEach((c) => element(by.css('input[type=password]')).sendKeys(c));
-    element(by.id('loginButton')).click();
-    browser.driver.sleep(1000);
-    
-    expect(element(by.css('.toolbar-title')).getText().then(text => expect(text).toEqual('About')));
-    });
 
   it('the user can browse to the contact tab and view the ionic twitter handle', () => {
  
